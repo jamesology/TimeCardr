9952180d152c8d9db5e5a0e93ed00528cc45cc56
James G <james.gerstmann@gmail.com>
2015-03-03 20:57:43 -0600

    i'm not exactly sure.... tests?

---
+++ TimeCardr.Tests/ConfiguratorTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+using log4net;
+using NUnit.Framework;
+using Shouldly;
+
+namespace TimeCardr.Tests
+{
+	[TestFixture]
+	class ConfiguratorTests
+	{
+		private ILog log;
+
+		[SetUp]
+		public void Setup()
+		{
+			log = LogManager.GetLogger("test");
+		}
+
+		[Test]
+		public void Initialize_EmptyArguments_NoValuesSet()
+		{
+			var args = new string[0];
+
+			var actual = Configurator.Initialize(args, log);
+
+			actual.Projects.ShouldBeEmpty();
+			actual.Tasks.ShouldBeEmpty();
+			actual.TimesheetFile.ShouldBeEmpty();
+		}
+	}
+}
